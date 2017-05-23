135ecf456645f0e7599e2be0235f06fc393feb47
Juan Carlos Condori Machicado <jcondori@agetic.gob.bo>
2017-05-23 09:47:13 -0400

    modificaciones para mostrar todo el arbol de la subclase

--- src/routes/api/clasificadores.js
+++ src/routes/api/clasificadores.js
@@ -26,15 +26,11 @@ module.exports = app => {
         where: ["descripcion LIKE ? AND tipo = 'SUBCLASE'", `%${req.query.des}%` ]
       })
       .then(pClasificadores => {
-        if (pClasificadores) {
-
+        if (pClasificadores.length > 0) {
           obtenerPadres(Clasificador, pClasificadores[0].dataValues.codigo)
           .then((respuesta) => {
-            console.log(respuesta)
+            res.status(200).send(util.formatearMensaje('EXITO', 'Obtencion de datos exitoso', { total: pClasificadores.length, datos: respuesta }))
           })
-
-          //console.log(obtenerPadres (pClasificadores[0].dataValues.codigo))
-          res.status(200).send(util.formatearMensaje('EXITO', 'Obtencion de datos exitoso', { total: pClasificadores.length, datos: pClasificadores }))
         } else {
           res.status(200).send(util.formatearMensaje('EXITO', 'No hay datos', { total: 0, datos: [] }))
         }
@@ -69,12 +65,16 @@ module.exports = app => {
     })
   })
 }
+
+//functions iterarResultados (p)
+
 function obtenerPadres (pModel, pCodSubClase) {
-  const util = require('../../lib/util')
   return new Promise((resolve, reject) => {
     let subclase = null
     let clase = null
     let grupo = null
+    let division = null
+    let seccion = null
     pModel.findOne(
       {
         where: {
@@ -85,6 +85,10 @@ function obtenerPadres (pModel, pCodSubClase) {
     )
     .then(pSubClase => {
       subclase = pSubClase.dataValues
+      // {
+      //   codigo: pSubClase.dataValues.codigo,
+      //   descripcion: pSubClase.dataValues.descripcion
+      // }
     })
     .then(pSubClase => {
       pModel.findOne(
@@ -96,7 +100,11 @@ function obtenerPadres (pModel, pCodSubClase) {
         }
       )
       .then(pClase => {
-        clase = pClase.dataValues
+          clase = pClase.dataValues
+          // {
+          //   codigo: pClase.dataValues.codigo,
+          //   descripcion: pClase.dataValues.descripcion
+          // }
       })
       .then(() => {
         pModel.findOne(
@@ -109,13 +117,73 @@ function obtenerPadres (pModel, pCodSubClase) {
         )
         .then(pGrupo => {
           grupo = pGrupo.dataValues
-          console.log(grupo)
+          // {
+          //   codigo: pGrupo.dataValues.codigo,
+          //   descripcion: pGrupo.dataValues.descripcion
+          // }
         })
         .then(() => {
-          return resolve({
-            grupo: grupo,
-            clase: clase,
-            subclase: subclase
+          pModel.findOne(
+            {
+              where: {
+                codigo: grupo.codigo_padre,
+                tipo: 'DIVISION'
+              }
+            }
+          )
+          .then(pDivision => {
+            division = pDivision.dataValues
+            // {
+            //   codigo: pDivision.dataValues.codigo,
+            //   descripcion: pDivision.dataValues.descripcion
+            // }
+          })
+          .then(() => {
+            pModel.findOne(
+              {
+                where: {
+                  codigo: division.codigo_padre,
+                  tipo: 'SECCION'
+                }
+              }
+            )
+            .then(pSeccion => {
+              seccion = pSeccion.dataValues
+              // {
+              //   codigo: pSeccion.dataValues.codigo,
+              //   descripcion: pSeccion.dataValues.descripcion
+              // }
+            })
+            .then(() => {
+              return resolve({
+                seccion: {
+                  codigo: seccion.codigo,
+                  descripcion: seccion.descripcion
+                },
+                division: {
+                  codigo: division.codigo,
+                  descripcion: division.descripcion
+                },
+                grupo: {
+                  codigo: grupo.codigo,
+                  descripcion: grupo.descripcion
+                },
+                clase: {
+                  codigo: clase.codigo,
+                  descripcion: clase.descripcion
+                },
+                subclase: {
+                  codigo: subclase.codigo,
+                  descripcion: subclase.descripcion
+                }
+              })
+            })
+            .catch(pError => {
+              console.log('Revisando el Error', pError)
+            })
+          })
+          .catch(pError => {
+            console.log('Revisando el Error', pError)
           })
         })
         .catch(pError => {
@@ -124,12 +192,10 @@ function obtenerPadres (pModel, pCodSubClase) {
       })
       .catch(pError => {
         console.log('Revisando el Error', pError)
-        //res.status(412).send(util.formatearMensaje('ERROR', pError))
       })
     })
     .catch(pError => {
       console.log('Revisando el Error', pError)
-      //res.status(412).send(util.formatearMensaje('ERROR', pError))
     })
   })
 }
